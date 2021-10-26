60c0ba3bcd5a2623f59de4b1c87d1cb0bf2e7b1c
Marcos <mrcsantos1@outlook.com>
2021-10-26 20:04:03 -0300

    start

---
+++ src/main/java/marcos/youtube/esp32server/Esp32ServerApplication.java
@@ -0,0 +1,13 @@
+package marcos.youtube.esp32server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esp32ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esp32ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marcos/youtube/esp32server/Esp32ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package marcos.youtube.esp32server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esp32ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
